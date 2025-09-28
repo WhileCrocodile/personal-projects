7d96b098ed9c80024c10e547edace154f6308b34
Evan Liu <eliu2855@gmail.com>
2025-09-28 17:26:44 -0400

    Minor changes
    Updated screenreader and added more test images

--- waveplates_tracker/screenreader.py
+++ waveplates_tracker/screenreader.py
@@ -24,7 +24,7 @@ def screenshot_wuwa() -> Image.Image:
         None, "Wuthering Waves  "
     )  # Lagging spaces are not a mistake
     if not hwnd:
-        raise WindowNotExist("Couldn't find Wuthering Waves.")
+        raise WindowNotExist("Could not find a running Wuthering Waves window.")
     # if win32gui.GetForegroundWindow() != hwnd:
     #     return None
     x, y, x1, y1 = win32gui.GetClientRect(hwnd)
@@ -34,7 +34,8 @@ def screenshot_wuwa() -> Image.Image:
     return im
 
 
-def match_template(img: Image.Image, match_type="full") -> Image.Image | None:
+def template_match_waveplates(img: Image.Image, match_type="full") -> Image.Image | None:
+    '''Uses template matching to detect the waveplate UI icon based on screenshot examples.'''
     templates = {
         "full": "templates/template_full.png",
         "blue": "templates/template_blue.png",
@@ -50,26 +51,25 @@ def match_template(img: Image.Image, match_type="full") -> Image.Image | None:
         )
     template = cv2.imread(templates[match_type], 0)
 
-    w, h = template.shape[::-1]
-    res = cv2.matchTemplate(img_cv2, template, cv2.TM_CCOEFF_NORMED)
+    width, height = template.shape[::-1]
+    match_results = cv2.matchTemplate(img_cv2, template, cv2.TM_CCOEFF_NORMED)
     threshold = 0.7
-    loc = np.where(res >= threshold)
+    loc = np.where(match_results >= threshold)
     if len(loc[0]) < 1:
         return None
     for pt in zip(*loc[::-1]):
         x, y = pt[0], pt[1]
         break  # If multiple x, y pairs found, only take the first
     
-    img = Image.fromarray(img_cv2).convert("RGB")
-    left = x
-    upper = y
-    right = x + w
-    lower = y + h
+    left = (x) * (1/scale_factor)
+    upper = (y) * (1/scale_factor)
+    right = (x + width) * (1/scale_factor)
+    lower = (y + height) * (1/scale_factor)
     return img.crop((left, upper, right, lower))
 
 
-def crop_match(img: Image.Image) -> tuple[Image.Image, Image.Image]:
-    """Crop the image match returned from match_template to only waveplate information (based on fixed template proportions)."""
+def crop_template_match(img: Image.Image) -> tuple[Image.Image, Image.Image]:
+    """Crop the image match returned from template_match_waveplates to only waveplate information (manual crop based on fixed template proportions)."""
     width, height = img.size
     upper = height * 0.1
     lower = height * 0.9
@@ -85,19 +85,9 @@ def crop_match(img: Image.Image) -> tuple[Image.Image, Image.Image]:
 
     return (green_crop, blue_crop)
 
-
-def crop_to_top(img: Image.Image) -> Image.Image:
-    """Crops a screenshot to only the top half of the image."""
-    width, _ = img.size
-    left = 0
-    upper = 0
-    right = width
-    lower = width * 0.5
-    return img.crop((left, upper, right, lower))
-
-
-def crop_to_waveplates(img: Image.Image) -> Image.Image:
-    """Crops a full-sized image to only the top right portion containing the waveplate info."""
+def fixed_crop_full_screenshot(img: Image.Image) -> Image.Image:
+    """Crops a full-sized client window screenshot to only the top right portion containing the waveplate info.
+    Used as a fallback method when waveplate detection fails."""
     width, _ = img.size
     left = width * 0.5  # left boundary in middle
     upper = 0  # upper boundary at max height
@@ -105,19 +95,28 @@ def crop_to_waveplates(img: Image.Image) -> Image.Image:
     lower = width * 0.06  # crops to button size based on width
     return img.crop((left, upper, right, lower))
 
+def crop_reduce_area(img: Image.Image) -> Image.Image:
+    """Crops a full-sized client window screenshot to the top right quarter.
+    Used in the first pass to reduce search area."""
+    width, height = img.size
+    left = width * 0.5  # left boundary in middle
+    upper = 0  # upper boundary at max height
+    right = width  # crops out the map button on the right
+    lower = height * 0.5  # crops to button size based on width
+    return img.crop((left, upper, right, lower))
 
 def preprocess_img(img: Image.Image) -> Image.Image:
     """Image preprocessing to make OCR more accurate."""
     img = cv2.cvtColor(
         np.array(img), cv2.COLOR_BGR2GRAY
-    )  # convert to cv2 to apply filters
-    thresh = 200
+    )
+    thresh = 200 # brightness value cutoff for conversion into black/white pixel 
     img = cv2.threshold(img, thresh, 255, cv2.THRESH_BINARY)[
         1
     ]  # convert to black and white
     # cv2.imshow(img_name, testimg)
     # cv2.waitKey()
-    img = Image.fromarray(img).convert("RGB")  # back to PIL Image to feed into OCR
+    img = Image.fromarray(img).convert("RGB")
     return img
 
 
@@ -135,18 +134,21 @@ def waveplate_from_img(img: Image.Image) -> tuple[int, int]:
         return extracted
 
     # Image processing
-    match = match_template(img)
-    if match is not None: 
+    match = template_match_waveplates(crop_reduce_area(img)) # First try with reduced search area
+    if match is None: # Second try with full image
+        match = match = template_match_waveplates(img)
+
+    if match is not None: # Main method using template matching to detect waveplates 
         img = match
         img = preprocess_img(img)
-        green_img, blue_img = crop_match(img)
+        green_img, blue_img = crop_template_match(img)
         green_ocr = pytesseract.image_to_string(green_img)
         blue_ocr = pytesseract.image_to_string(blue_img)
         green_waveplates = extract(r"\d{1,3}", green_ocr)
         blue_waveplates = extract(r"(\d{1,3})\/240", blue_ocr)
         return (int(blue_waveplates), int(green_waveplates))
-    else: # Fallback method
-        img = crop_to_waveplates(img)
+    else: # Fallback method using a manual crop of the provided screenshot
+        img = fixed_crop_full_screenshot(img)
         img = preprocess_img(img)
         # OCR
         ocr_text = pytesseract.image_to_string(img)
@@ -187,13 +189,13 @@ def main():
     for img_name in imgs:
         img_path = screenshots_path + img_name
         image = Image.open(img_path)
-        # image = match_template(image)
+        # image = template_match_waveplates(image)
 
         # if image is None:
         #     print(f"Matching failed on {img_name}")
         # else:
         #     image.show()
-        # green_img, blue_img = crop_match(image)
+        # green_img, blue_img = crop_template_match(image)
         # green_img = preprocess_img(green_img)
         # blue_img = preprocess_img(blue_img)
         # green_img.show()
